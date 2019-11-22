fa5a55b76f3eacd1390167d2e6188588667304f8
Matt <mtrdazzo@gmail.com>
2019-11-21 16:34:06 -0800

    Remove main function, using namespace std from file

--- Chapter_7/Exercises/Ex_7_28.cpp
+++ Chapter_7/Exercises/Ex_7_28.cpp
@@ -1,6 +1,6 @@
-/* 
+/*
  * Exercise 7-28 Palindromic Sequences
- * 
+ *
  * A palindrome is a string that's spelled the same way forward and backward.
  * Examples of palindromes include "radar" and "Able was I ere I saw Elba." A
  * palindromic sentences is a palindrome if the punctuation marks and spaces
@@ -10,57 +10,30 @@
  * sir, on nurses order." is considered a palindrome. Note that like an array, the
  * square brackets ([]) operator can be used to iterate through the characters in
  * a string.
- * 
+ *
  */
 
 #include <iostream>
 #include <array>
-using namespace std;
-
-bool isPalindrome(string&, size_t, size_t);
-
-int main(int argc, char **argv) {
-
-    array<string, 7> palindrome_strings{ "",  /* empty string */
-                                   "aa", /* single character string */
-                                   "a",  /* even numbered character string */
-                                   "radar", /* odd numbered character string */
-                                   "Able was I ere I saw Elba.", /* palindrome with capitols */
-                                   "red roses run no risk, sir, on nurses order." /* palindrome sentence with punctuation even */
-                                   "red roses run no risk, sir, on nurses order." }; /* palindrome sentence with punctuation odd */
-
-    array<string, 4> non_palindrome_strings{ "ab", /* even character string */
-                                             "raars", /* odd numbered character string */
-                                             "Able was I sere I saw Elba.", /* palindrome with capitols */
-                                             "red roses run lno risk, sir, on nurses order."}; /* palindrome sentence with punctuation even */
-
-    for (size_t index{0}; index < palindrome_strings.size(); index++)
-        cout << isPalindrome(palindrome_strings[index], 0, palindrome_strings[index].size()) << endl;
-
-    for (size_t index{0}; index < non_palindrome_strings.size(); index++)
-        cout << isPalindrome(non_palindrome_strings[index], 0, non_palindrome_strings[index].size()) << endl;
-
-    return EXIT_SUCCESS;
-}
 
 /* isPalindrome: recursive solution to check if a string str is a palindrome */
-bool isPalindrome(string &str, size_t beg, size_t end) {
-    
+bool isPalindrome(std::string &str, size_t b_index, size_t e_index) {
+
     /* Exit criteria */
-    if (beg > end)
+    if (b_index > e_index)
         return true;
-    else if (beg == end)
-        return tolower(str[beg]) == tolower(str[end-1]);
+    else if (b_index == e_index)
+        return tolower(str[b_index]) == tolower(str[e_index-1]);
 
     /* Skip through non alpha characters */
-    while (!isalpha(str[beg]) && beg < end)
-        ++beg;
-    while (!isalpha(str[end-1]) && end - 1 >= 0)
-        --end;
-    
+    while (!isalpha(str[b_index]) && b_index < e_index)
+        ++b_index;
+    while (!isalpha(str[e_index-1]) && e_index - 1 >= 0)
+        --e_index;
+
     /* Recursive call */
-    if (tolower(str[beg]) == tolower(str[end-1]))
-        return isPalindrome(str, beg+1, end-1);
+    if (tolower(str[b_index]) == tolower(str[e_index-1]))
+        return isPalindrome(str, b_index+1, e_index-1);
     else
         return false;
 }
