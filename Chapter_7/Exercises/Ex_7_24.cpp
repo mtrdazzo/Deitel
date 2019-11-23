a9f04d2905167af993cc90651f3de1726a96f232
Matt <mtrdazzo@gmail.com>
2019-11-22 16:57:09 -0800

    Initial commit of eight queens puzzle

---
+++ Chapter_7/Exercises/Ex_7_24.cpp
@@ -0,0 +1,8 @@
+/**
+ * Exercise 7-24 Eight Queens
+ *
+ * Is it possible to place eight queens on an empty chessboard so that no
+ * queen is "attacking" any other, i.e., no two queens are in the same row,
+ * the same column, or along the same diagonal?
+ *
+ */
