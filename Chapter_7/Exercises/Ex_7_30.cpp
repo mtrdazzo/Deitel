d0e45d657b109b58e5814d433fa5fe09026b7612
Matt <mtrdazzo@gmail.com>
2019-11-22 20:52:14 -0800

    Adding solution to printArray with header file

---
+++ Chapter_7/Exercises/Ex_7_30.cpp
@@ -0,0 +1,31 @@
+/**
+ * Exercise 7-30 Print an array
+ *
+ * Write a recursive function printArray that takes an array, a starting subscript
+ * and an ending subscript as arguments, returns nothing and prints the array. The
+ * function should stop processing and return when the starting subscript equals
+ * the ending subcript.
+ *
+ */
+
+#include <iostream>
+#include <array>
+#include "Ex_7_30.h"
+
+/**
+ * Recursively prints the elements of an array in reverse.
+ *
+ * @param arr     Example array type with 10 integers
+ * @param b_index Beginning index of the array
+ * @param e_index Ending index of the array
+ * @return void
+ */
+void printArray(example_array& arr, size_t b_index, size_t e_index) {
+    /* empty array */
+    if (arr.size() == 0)
+        return;
+
+    if (b_index < e_index)
+        printArray(arr, b_index+1, e_index);
+    std::cout << arr[b_index] << " ";
+}
---
+++ Chapter_7/Exercises/Ex_7_30.h
@@ -0,0 +1,10 @@
+#ifndef _EX_7_30_H
+#define _EX_7_30_H
+
+#define SIZE_ARRAY 10
+
+typedef std::array<int, SIZE_ARRAY> example_array;
+
+void printArray(example_array&, size_t, size_t);
+
+#endif /* _EX_7_30_H */
