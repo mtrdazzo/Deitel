e1d657ae3d53ae32bb18eab4ea273abdc37f3a54
mtrdazzo <mtrdazzo@gmail.com>
2020-03-30 17:15:09 -0700

    Adding TieGame test

--- Chapter_9/Tests/tst_Ex_9_15.cpp
+++ Chapter_9/Tests/tst_Ex_9_15.cpp
@@ -4,15 +4,28 @@
 /**
  * @brief Create TicTacToe game and start
  */
-TEST(TwoPlayerGame, StartGame) {
+TEST(TwoPlayerSimulatedGame, TieGame) {
 
     std::fstream playerMoves;
-    const char *fileName = "inputFile.txt";
+    const char *fileName = "moves.txt";
 
     playerMoves.open(fileName, std::ios::out);
-    playerMoves << "2 1 2 3 4 6 5 7 8 9";
+    playerMoves << "2 1 2 3 4 6 5 7 9 8";
     playerMoves.close();
 
     TicTacToe ttt(fileName);
+    EXPECT_EQ(ttt.m_uiNumPlayers, 2);
+
+    EXPECT_EQ(ttt.m_pBoard[0][0], TicTacToe::PLAYER_1);
+    EXPECT_EQ(ttt.m_pBoard[0][1], TicTacToe::PLAYER_2);
+    EXPECT_EQ(ttt.m_pBoard[0][2], TicTacToe::PLAYER_1);
+    EXPECT_EQ(ttt.m_pBoard[1][0], TicTacToe::PLAYER_2);
+    EXPECT_EQ(ttt.m_pBoard[1][1], TicTacToe::PLAYER_2);
+    EXPECT_EQ(ttt.m_pBoard[1][2], TicTacToe::PLAYER_1);
+    EXPECT_EQ(ttt.m_pBoard[2][0], TicTacToe::PLAYER_1);
+    EXPECT_EQ(ttt.m_pBoard[2][1], TicTacToe::PLAYER_1);
+    EXPECT_EQ(ttt.m_pBoard[2][2], TicTacToe::PLAYER_2);
+    EXPECT_EQ(ttt._findWinner(), TicTacToe::TIE_GAME);
+    
     std::remove(fileName);
 }
