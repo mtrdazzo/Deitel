9f83a6b56234e589a8a8a981f78dd1e0e299a786
mtrdazzo <mtrdazzo@gmail.com>
2020-06-13 12:46:21 -0700

    Resturcturing tests and removing ostreams from test file

--- Chapter_10/Tests/tst_Ex_10_9.cpp
+++ Chapter_10/Tests/tst_Ex_10_9.cpp
@@ -2,78 +2,69 @@
 #include <sstream>
 #include <Ex_10_9.h>
 
+
 /**
- * @brief Create single digit and double HugeIntegers using longs.
+ * @brief Instantiate HugeInteger with no arguments.
  * 
  */
-TEST(Constructor, SingleMultiDigitIntegers) {
-    
-    /* zero */
+TEST(Constructor, Default) {
+
     HugeInteger int1;
-    std::ostringstream output1;
 
-    output1 << int1;
-
-    EXPECT_STREQ(output1.str().c_str(), "0");
-
-    /* single digit */
-    HugeInteger int2{3};
-    std::ostringstream output2;
-
-    output2 << int2;
-
-    EXPECT_STREQ(output2.str().c_str(), "3");
-
-    /* multi digit */
-    HugeInteger int3{99};
-    std::ostringstream output3;
-
-    output3 << int3;
-
-    EXPECT_STREQ(output3.str().c_str(), "99");
-
-    /* maximum positive long */
-    HugeInteger int4{0x7FFFFFFFFFFFFFFF};
-    std::ostringstream output4;
-
-    output4 << int4;
-
-    EXPECT_STREQ(output4.str().c_str(), "9223372036854775807");
+    EXPECT_EQ(int1.str(), "0");
 }
 
 /**
- * @brief Create single digit and double HugeIntegers using strings.
+ * @brief Instantiate a HugeInteger using an integer.
  * 
  */
-TEST(Constructor, SingleMultiDigitStrings) {
+TEST(Constructor, Integer) {
+    
+    /* zero */
+    HugeInteger int1{0};
+
+    EXPECT_EQ(int1.str(), "0");
+
+    /* single digit */
+    int single_digit_int{3};
+    HugeInteger int2{single_digit_int};
+    EXPECT_EQ(int2.str(), std::to_string(single_digit_int));
+
+    /* multi digit */
+    int multi_digit_int{12345678};
+    HugeInteger int3{multi_digit_int};
+    EXPECT_EQ(int3.str(), std::to_string(multi_digit_int));
+
+    /* maximum positive long */
+    long int max_long{0x7FFFFFFFFFFFFFFF};
+    HugeInteger int4{max_long};
+    EXPECT_EQ(int4.str(), std::to_string(max_long));
+
+}
+
+/**
+ * @brief Instantiate a HugeInteger using a string.
+ * 
+ */
+TEST(Constructor, String) {
 
     /* single digit string */
     std::string singleDigitString{"1"};
     HugeInteger int1{singleDigitString};
-    std::ostringstream output1;
 
-    output1 << int1;
-
-    EXPECT_STREQ(output1.str().c_str(), singleDigitString.c_str());
-
+    EXPECT_EQ(int1.str(), singleDigitString.c_str());
 
     /* multi digit string */
     std::string multiDigitString{"1234234235"};
     HugeInteger int2{multiDigitString};
-    std::ostringstream output2;
 
-    output2 << int2;
-
-    EXPECT_STREQ(output2.str().c_str(), multiDigitString.c_str());
+    EXPECT_EQ(int2.str(), multiDigitString.c_str());
 
     /* max digit string */
     std::string maxDigitString{"1234567890123456789012345678901234567890"};
     HugeInteger int3{maxDigitString};
-    std::ostringstream output3;
 
-    output3 << int3;
-
-    EXPECT_STREQ(output3.str().c_str(), maxDigitString.c_str());
+    EXPECT_EQ(int3.str(), maxDigitString.c_str());
 
     /* invaild digit string middle */
     try {
@@ -104,10 +95,10 @@ TEST(Constructor, SingleMultiDigitStrings) {
 }
 
 /**
- * @brief Addition + operator overload with HugeIntegers
+ * @brief Addition operator overloaded with HugeIntegers
  * 
  */
-TEST(Operators, additionOnlyHugeIntegers) {
+TEST(Addition, HugeIntegers) {
 
     /* single digit string */
     HugeInteger int1{"5"};
@@ -146,10 +137,10 @@ TEST(Operators, additionOnlyHugeIntegers) {
 }
 
 /**
- * @brief Addition + operator overload with ints and HugeIntegers
+ * @brief Addition operator overload with Integers
  * 
  */
-TEST(Operators, additionWithInts) {
+TEST(Addition, Integers) {
 
     /* single digit add */
     HugeInteger int1{"5"};
@@ -188,10 +179,10 @@ TEST(Operators, additionWithInts) {
 }
 
 /**
- * @brief Addition + operator overload with strings and HugeIntegers
+ * @brief Addition operator overloaded with Strings
  * 
  */
-TEST(Operators, additionWithString) {
+TEST(Addition, Strings) {
 
     /* single digit add */
     HugeInteger int1{"5"};
@@ -230,10 +221,10 @@ TEST(Operators, additionWithString) {
 }
 
 /**
- * @brief Multiplication * operator with integers
+ * @brief Multiplication operator overloaded with integers
  * 
  */
-TEST(Operators, multiplicationWithInt) {
+TEST(Multiplication, Integers) {
 
     HugeInteger int1{3};
     HugeInteger int2{10};
@@ -241,8 +232,12 @@ TEST(Operators, multiplicationWithInt) {
 
     /* Single digit identity */
     int3 = int1 * 1;
-    EXPECT_EQ(int3.str(), "3");
+    EXPECT_EQ(int3.str(), int1.str());
  
+    /* Double digit identity */
+    int3 = int2 * 1;
+    EXPECT_EQ(int3.str(), int2.str());
+
     /* Single digit zero multiplier */
     int3 = int1 * 0;
     EXPECT_EQ(int3.str(), "0");
@@ -255,35 +250,21 @@ TEST(Operators, multiplicationWithInt) {
     int3 = int1 * 2;
     EXPECT_EQ(int3.str(), "6");
 
-    /* Single digit with carry */
-    int3 = int1 * 4;
-    EXPECT_EQ(int3.str(), "12");
-
-    /* Double digit identity */
-    int3 = int3 * 1;
-    EXPECT_EQ(int3.str(), "12");
-
-    /* Double digit zero multiplier */
-    int3 = int3 * 0;
-    EXPECT_EQ(int3.str(), "0");
-
     /* Double digit with no carry */
     int3 = int2 * 10;
     EXPECT_EQ(int3.str(), "100");
 
-    int2 = 15;
-    int3 = int2 * 15;
+    /* Single digit with carry */
+    int3 = int1 * 4;
+    EXPECT_EQ(int3.str(), "12");
 
     /* Double digit with carry */
+    int2 = 15;
+    int3 = int2 * 15;
     EXPECT_EQ(int3.str(), "225");
 
-}
-
-TEST(operators, multiplicationAdvancedInt) {
-
-    HugeInteger int1{12345};
-    int int2{3456};
-    HugeInteger int3 = int1 * int2;
-
-    EXPECT_EQ(int3.str(), "42664320");
+    /* Maximum long multiplication */
+    int1 = 0x7FFFFFFF;
+    int3 = int1 * 0x7FFFFFFF;
+    EXPECT_EQ(int3.str(), "4611686014132420609");
 }
