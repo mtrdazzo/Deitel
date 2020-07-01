1d3a0329e597ff17e59b7a76c9b42e0ea2a34c21
mtrdazzo <mtrdazzo@gmail.com>
2020-06-30 17:55:35 -0700

    Initial implementation of the Polynomial class definition

---
+++ Chapter_10/Exercises/Ex_10_11.h
@@ -0,0 +1,97 @@
+/**
+ * @file Ex_10_11.h
+ * @author Matthew J Randazzo (mtrdazzo@gmail.com)
+ * @brief Polynomial Class definition
+ * 
+ * Exercise 10-11 Polynomial Class
+ * 
+ * Develop class Polynomial. The internal representation of a Polynomial is an array of
+ * terms. Each term contains a coefficient and an exponent-e.g.the term 2x^4 has the coefficient
+ * 2 and the exponent 4. Develop a complete class containing proper constructor and destructor
+ * functions as well as set and get functions. The class houdl also provide the following
+ * overloaded operator capabilities:
+ * 
+ *  a) Overload the addition operator (+) to add two Polynomials.
+ *  b) Overload the subtraction operator (-) to subtract two Polynomials.
+ *  c) Overload the assignment operator to assign one Polynomial to another.
+ *  d) Overload the multiplication operator (*) to multiply two Polynomials.
+ *  e) Overload the addition assignment operator (+=), subtraction assignment operator (-=), and
+ *     multiplication assignment operator (*=).
+ * 
+ * @version 0.1
+ * @date 2020-06-30
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <cstddef>
+
+#define NUM_COFFICIENTS 40
+
+class Polynomial {
+    public:
+
+        /**
+         * @brief Assignment operator.
+         * 
+         * @return Polynomial 
+         */
+        Polynomial operator=(const Polynomial &) const;
+
+        /**
+         * @brief Addition operator.
+         * 
+         * @return Polynomial 
+         */
+        Polynomial operator+(const Polynomial &) const;
+
+        /**
+         * @brief Subtraction operator.
+         * 
+         * @return Polynomial 
+         */
+        Polynomial operator-(const Polynomial &) const;
+
+        /**
+         * @brief Multiplication operator.
+         * 
+         * @return Polynomial 
+         */
+        Polynomial operator*(const Polynomial &) const;
+
+        /**
+         * @brief Addition assignment operator.
+         * 
+         */
+        void operator+=(const Polynomial &);
+
+        /**
+         * @brief Subtraction assignment operator.
+         * 
+         */
+        void operator-=(const Polynomial &);
+
+        /**
+         * @brief Multiplication assignment operator.
+         * 
+         */
+        void operator*=(const Polynomial &);
+
+        /**
+         * @brief Get the coefficient C of the nth polynomial of the form C*x^n.
+         * 
+         * @return int 
+         */
+        int getCoefficient(size_t) const;
+
+        /**
+         * @brief Set the coefficient C of the nth polynomial of the form C*x^n.
+         * 
+         */
+        void setCoefficient(int);
+
+    private:
+
+        int m_iCoefficients[NUM_COFFICIENTS];
+}
