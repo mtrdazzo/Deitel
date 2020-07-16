b0440a18a09a963647c3f2195476bb363a2177ba
mtrdazzo <mtrdazzo@gmail.com>
2020-07-15 22:22:43 -0700

    Adding tests for SalariedEmployee

--- Chapter_12/Tests/tst_Ex_12_14.cpp
+++ Chapter_12/Tests/tst_Ex_12_14.cpp
@@ -181,6 +181,102 @@ TEST(CommissionEmployee, GettersSetters) {
     EXPECT_EQ(outputExepected.str(), ce.toString());
 }
 
+TEST(SalariedEmployee, Constructor) {
+
+    /* Sunny Day scenario */
+    SalariedEmployee ce{"Jon", "Doe", "123456789", 4, 8, 1989, 5000};
+
+    /* empty first name */
+    try {
+        SalariedEmployee ce{"", "Doe", "123456789", 4, 8, 1989, 1.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid first name");
+    }
+
+    /* empty last name */
+    try {
+        SalariedEmployee ce{"Jon", "", "123456789", 4, 8, 1989, 1.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid last name");
+    }
+
+    /* invalid ssn */
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "12345678", 4, 8, 1989, 1.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid ssn, must be nine digits");
+    }
+
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "1234567890", 4, 8, 1989, 1.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid ssn, must be nine digits");
+    }
+
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "12-456789", 4, 8, 1989, 1.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid ssn, must be nine digits");
+    }
+
+    /* Invalid salary */
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "123456789", 4, 8, 1989, 0.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid salary, must be greater than 0.0");
+    }
+
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "123456789", 4, 8, 1989, -0.1};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid salary, must be greater than 0.0");
+    }
+
+    /* Invalid salary */
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "123456789", 4, 8, 1989, 0.0};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid salary, must be greater than 0.0");
+    }
+
+    try {
+        SalariedEmployee ce{"Jon", "Doe", "123456789", 4, 8, 1989, -1000};
+    } catch (std::invalid_argument & err) {
+        EXPECT_STREQ(err.what(), "invalid salary, must be greater than 0.0");
+    }
+}
+
+TEST(SalariedEmployee, GettersSetters) {
+
+    std::string firstName{"JonNameThatsTooLong"};
+
+    SalariedEmployee ce{"Jon", "Doe", "123456789", 4, 8, 1989, 1000};
+    EXPECT_EQ(ce.getFirstName(), "Jon");
+
+    /* First Name too long */
+    ce.setFirstName(firstName);
+    EXPECT_EQ(ce.getFirstName(), firstName);
+
+    /* Last Name too long */
+    EXPECT_EQ(ce.getLastName(), "Doe");
+    ce.setLastName(firstName);
+    EXPECT_EQ(ce.getLastName(), firstName);
+
+    /* Getters */
+    EXPECT_EQ(ce.getSocialSecurityNumber(), "123456789");
+    EXPECT_EQ(ce.getWeeklySalary(), 1000);
+
+    EXPECT_EQ(ce.earnings(), 1000);
+
+    std::ostringstream outputExepected;
+    outputExepected << std::fixed << std::setprecision(2);
+    outputExepected << "salaried employee: " + ce.getFirstName() + " " + ce.getLastName() + '\n';
+    outputExepected << "social security number: " << ce.getSocialSecurityNumber() << '\n';
+    outputExepected << "weekly salary: " << ce.getWeeklySalary();
+
+    EXPECT_EQ(outputExepected.str(), ce.toString());
+}
+
 TEST(BasePlusCommissionEmployee, Constructor) {
 
     /* Sunny Day scenario */
