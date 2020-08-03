5336bd50b40e5e4330ffd1705ed162946fb90c0c
mtrdazzo <mtrdazzo@gmail.com>
2020-08-02 18:00:18 -0700

    Adding solution and implementation for Point class

--- Chapter_13/Exercises/Ex_13_15.cpp
+++ Chapter_13/Exercises/Ex_13_15.cpp
@@ -8,3 +8,120 @@
  * @copyright Copyright (c) 2020
  * 
  */
+
+#include <limits>
+#include <stdexcept>
+#include <sstream>
+#include "Ex_13_15.h"
+
+/**
+ * @brief Construct a new Point:: Point object
+ * 
+ * @param xCoordiante 
+ * @param yCoordinate 
+ */
+Point::Point(int xCoordinate, int yCoordinate) {
+    setXCoordinate(xCoordinate);
+    setYCoordinate(yCoordinate);
+}
+
+/**
+ * @brief Output Two dimensional point to stream
+ * 
+ * @param output Output stream
+ * @param point  Point object
+ * @return std::ostream& Output stream for cascading
+ */
+std::ostream& operator<<(std::ostream& output, const Point& point) {
+
+    output << "xCoordinate: " << point.xCoordinate;
+    output << "\nyCoordinate: " << point.yCoordinate;
+
+    return output;
+}
+
+/**
+ * @brief Input integers from stream to point
+ * 
+ * @param input Input stream
+ * @param point Point object
+ * @return std::istream& Input stream for cascading
+ */
+std::istream& operator>>(std::istream& input, Point & point) {
+
+    static char inputArray[MAX_INPUT_LEN] = {0};
+    bool isNegative{false};
+    int index{0};
+    long tempValue{0};
+
+    input.get(inputArray, MAX_INPUT_LEN, '\n');
+
+    if (inputArray[0] == '-') {
+        isNegative = true;
+        ++index;
+    }
+
+    for (int tempIndex{index}; isdigit(inputArray[tempIndex]); ++tempIndex, ++index) {
+        tempValue = tempValue * 10 + inputArray[tempIndex] - '0';
+    }
+
+    if (isNegative)
+        tempValue *= -1;
+
+    if (inputArray[index] != ' ')
+        throw std::invalid_argument("invalid character");
+    else if (tempValue < std::numeric_limits<int>::min())
+        throw std::invalid_argument("value too small");
+    else if (tempValue > std::numeric_limits<int>::max())
+        throw std::invalid_argument("value too large");
+    else {
+        point.setXCoordinate(static_cast<int>(tempValue));
+    }
+
+    if (input.gcount() > index)
+        while (inputArray[index] == ' ')
+            ++index;
+    else {
+        throw std::invalid_argument("no second value");
+    }
+
+    if (inputArray[index] == '-') {
+        isNegative = true;
+        ++index;
+    } else {
+        isNegative = false;
+    }
+
+    tempValue = 0;
+    for (int tempIndex{index}; isdigit(inputArray[tempIndex]); ++tempIndex, ++index) {
+        tempValue = tempValue * 10 + inputArray[tempIndex] - '0';
+    }
+
+    if (isNegative)
+        tempValue *= -1;
+
+    if (inputArray[index] != '\0')
+        throw std::invalid_argument("invalid character");
+    else if (tempValue < std::numeric_limits<int>::min())
+        throw std::invalid_argument("value too small");
+    else if (tempValue > std::numeric_limits<int>::max())
+        throw std::invalid_argument("value too large");
+    else
+        point.setYCoordinate(static_cast<int>(tempValue));
+
+    return input;
+}
+
+/**
+ * @brief Return the string representation of the Point
+ * 
+ * @return std::string 
+ */
+std::string Point::toString() const {
+
+    std::ostringstream output;
+
+    output << *this;
+
+    return output.str();
+}
--- Chapter_13/Exercises/Ex_13_15.h
+++ Chapter_13/Exercises/Ex_13_15.h
@@ -26,7 +26,61 @@
 #ifndef EX_13_15_H
 #define EX_13_15_H
 
+#include <iostream>
+#define MAX_INPUT_LEN 50
+
 class Point {
+
+    /* Stream insertion and extraction operators */
+    friend std::ostream& operator<<(std::ostream&, const Point&);
+    friend std::istream& operator>>(std::istream&, Point&);
+
+    public:
+
+        /**
+         * @brief Construct a new Point object
+         * 
+         */
+        Point(int = 0, int = 0);
+
+        /**
+         * @brief Return the X Coordinate
+         * 
+         * @return int 
+         */
+        int getXCoordinate() const { return xCoordinate; }
+
+        /**
+         * @brief Set the X Coordinate
+         * 
+         * @param xCoordinate 
+         */
+        void setXCoordinate(int xCoordinate) { this->xCoordinate = xCoordinate; }
+
+        /**
+         * @brief Return the Y Coordinate
+         * 
+         * @return int 
+         */
+        int getYCoordinate() const { return yCoordinate; }
+
+        /**
+         * @brief Set the Y Coordinate
+         * 
+         * @param yCoordinate 
+         */
+        void setYCoordinate(int yCoordinate) { this->yCoordinate = yCoordinate; }
+
+        /**
+         * @brief Return the string representation of the Point
+         * 
+         * @return std::string 
+         */
+        std::string toString() const;
+
+    private:
+        int xCoordinate;
+        int yCoordinate;
 
 };
 
