3854d8f92c7acaeb8f8db766916d2aafc2728ced
mtrdazzo <mtrdazzo@gmail.com>
2020-08-09 21:04:19 -0700

    Adding implementation and test for statistics class

---
+++ Chapter_13/Exercises/Ex_13_17.cpp
@@ -0,0 +1,171 @@
+/**
+ * @file Ex_13_17.cpp
+ * @author Matthew J Randazzo (mtrdazzo@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-08-09
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include "Ex_13_17.h"
+#include <limits>
+#include <stdexcept>
+
+/**
+ * @brief Stream extraction operator to input values to integer vector
+ * 
+ * @param input Input stream
+ * @param stats Statistics object
+ * @return std::istream& Input stream for cascading calls
+ */
+std::istream& operator>>(std::istream& input, Statistics &stats) {
+
+    char inputArray[100];
+    long tempValue{0};
+    bool numberStarted{false};
+    bool isNegative{false};
+
+    input.get(inputArray, 100, '\n');
+
+    while (!stats._isEmpty())
+        stats.numbers.pop_back();
+
+    for (int index{0}; index < input.gcount(); ++index) {
+        if (inputArray[index] == ' ') {
+            if (numberStarted) {
+                if (isNegative)
+                    tempValue *= -1;
+                stats.numbers.push_back(tempValue);
+                tempValue = 0;
+                numberStarted = false;
+                isNegative = false;
+            }
+        }
+        else if (isdigit(inputArray[index])) {
+            if (!numberStarted)
+                numberStarted = true;
+            tempValue *= 10;
+            tempValue += inputArray[index] - '0';
+
+            if (!isNegative && tempValue > std::numeric_limits<int>::max())
+                throw std::invalid_argument("value too large");
+            else if (isNegative && tempValue * -1 < std::numeric_limits<int>::min())
+                throw std::invalid_argument("value too small");
+        }
+        else if (inputArray[index] == '-') {
+            if (!isNegative) {
+                isNegative = true;
+            }
+            else
+            {
+                throw std::invalid_argument("invalid symbol");
+            }
+        }
+        else {
+            throw std::invalid_argument("invalid character");
+        }
+    }
+
+    if (numberStarted) {
+        if (!isNegative) {
+            if ( tempValue > std::numeric_limits<int>::max())
+                throw std::invalid_argument("value too large");
+        }
+        else {
+            tempValue *= -1;
+            if (tempValue < std::numeric_limits<int>::min())
+                throw std::invalid_argument("value too small");
+        }
+        stats.numbers.push_back(tempValue);
+    }
+
+    stats._bubbleSort();
+
+    return input;
+}
+
+/**
+ * @brief Get the minimum number in the vector
+ * 
+ * @return int 
+ */
+int Statistics::min() const {
+
+    if (_isEmpty())
+        throw std::invalid_argument("empty vector");
+    else
+    {
+        return numbers.front();
+    }
+}
+
+/**
+ * @brief Get the maximum number in the vector
+ * 
+ * @return int 
+ */
+int Statistics::max() const {
+
+    if (_isEmpty())
+        throw std::invalid_argument("empty vector");
+    else
+        return numbers.back();
+}
+
+/**
+ * @brief Return the mean of the number set in the vector
+ * 
+ * @return double 
+ */
+double Statistics::mean() const {
+
+    if (_isEmpty())
+        throw std::invalid_argument("empty vector");
+
+    long total{0};
+
+    for (auto number: numbers)
+        total += number;
+    
+    return static_cast<double>(total) / numbers.size();
+}
+
+/**
+ * @brief Calculate the median of the number set in the vector
+ * 
+ * @return double 
+ */
+double Statistics::median() const {
+
+    if (_isEmpty())
+        throw std::invalid_argument("empty vector");
+
+    size_t numbersSize{numbers.size()};
+
+    if (numbersSize % 2)
+        return static_cast<double>(numbers[numbersSize / 2 ]);
+    else
+    {
+        return static_cast<double>(numbers[numbersSize / 2] + numbers[numbersSize / 2 - 1]) / 2;
+    }
+}
+
+/**
+ * @brief 
+ * 
+ */
+void Statistics::_bubbleSort() {
+
+    int temp;
+    for (size_t index{0}; index < numbers.size() - 1; ++index) {
+        for (size_t index_other{0}; index_other < (numbers.size() - 1 - index); ++index_other) {
+            if (numbers[index_other] > numbers[index_other + 1]) {
+                temp = numbers[index_other];
+                numbers[index_other] = numbers[index_other + 1];
+                numbers[index_other + 1] = temp;
+            }
+        }
+    }
+}
---
+++ Chapter_13/Exercises/Ex_13_17.h
@@ -0,0 +1,78 @@
+/**
+ * @file Ex_13_17.h
+ * @author Matthew J Randazzo (mtrdazzo@gmail.com)
+ * @brief Statistics class definition
+ * @version 0.1
+ * @date 2020-08-09
+ * 
+ * Exercise 13-17 Computing descriptive statistics for a data set
+ * 
+ * Write a program that reads a sequence of numbers from the user into a vector and then calculates the mean, median,
+ * standard deviation, minimum, maximum of the numbers. Consult a statistics book for formulas for mean, mode,
+ * and standard deviation. The program should display the numbers in two digits of precision.
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef EX_13_17_H
+#define EX_13_17_H
+
+#include <iostream>
+#include <vector>
+
+class Statistics {
+
+    friend std::istream& operator>>(std::istream&, Statistics &);
+
+    public:
+
+        /**
+         * @brief Construct a new Statistics object
+         * 
+         */
+        Statistics() = default;
+
+        /**
+         * @brief Calculate the mean of the numbers
+         * 
+         * @return double 
+         */
+        double mean() const;
+
+        /**
+         * @brief Calculate the median of the numbers
+         * 
+         * @return double 
+         */
+        double median() const;
+
+        /**
+         * @brief Calculate the standard deviation of the numbers
+         * 
+         * @return double 
+         */
+        double standardDeviation();
+
+        /**
+         * @brief Return the minimum of the numbers
+         * 
+         * @return int 
+         */
+        int min() const;
+
+        /**
+         * @brief Return the maximum of the numbers
+         * 
+         * @return int 
+         */
+        int max() const;
+
+    private:
+        std::vector<int> numbers;
+
+        bool _isEmpty() const { return !numbers.size(); }
+        void _bubbleSort();
+};
+
+#endif /* EX_13_17_H */
